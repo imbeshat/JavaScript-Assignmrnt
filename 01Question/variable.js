6730887d3cdee8903ec88c1865090164a1b8c128
Imbeshat <md.imbeshat@gmail.com>
2023-01-11 00:35:10 +0530

    added solution for question1

---
+++ 01Question/variable.js
@@ -0,0 +1,9 @@
+let str = "I am String";
+let bool = false;
+let undef = undefined;
+let nul = null;
+
+console.log(`Value of str = ${str} and data type of str = ${typeof str}`);
+console.log(`Value of bool = ${bool} and data type of bool = ${typeof bool}`);
+console.log(`Value of undef = ${undef} and data type of undef = ${typeof undef}`);
+console.log(`Value of nul = ${nul} and data type of nul = ${typeof nul}`);
