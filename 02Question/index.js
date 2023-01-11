59e13f21a605a0ae8693061f493b07d8c62bf35e
Imbeshat <md.imbeshat@gmail.com>
2023-01-11 16:55:18 +0530

    added solution for question2

---
+++ 02Question/index.js
@@ -0,0 +1,7 @@
+let fName = "Imbeshat";
+let lName = "Aslam";
+let maritalStatus = "Single";
+let country = "India";
+let age = 25;
+
+console.log(`I am ${fName} ${lName} from ${country}. I am ${age} years old and I am ${maritalStatus}.`);
