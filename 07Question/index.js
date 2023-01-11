8a68e94879c6030b62a6a6547292048d8e640174
Imbeshat <md.imbeshat@gmail.com>
2023-01-11 18:13:56 +0530

    added solution for question7

---
+++ 07Question/index.js
@@ -0,0 +1,5 @@
+let company = ["pw skills", "ineuron", "pw skills", "learn code online", "linkedin"];
+
+console.log(company);
+console.log("First occurence of pw skills: ", company.indexOf("pw skills"));
+console.log("Last occurence of pw skills: ", company.lastIndexOf("pw skills"));
