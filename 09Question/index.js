2be979e98fe2aae835c90ad4f38f7eee030a1b95
Imbeshat <md.imbeshat@gmail.com>
2023-01-11 19:12:59 +0530

    added solution for question9

---
+++ 09Question/index.js
@@ -0,0 +1,9 @@
+console.log("------Truthy Values------");
+console.log(Boolean(true));
+console.log(Boolean([]));
+console.log(Boolean(new Date()));
+
+console.log("------Falsy Values------");
+console.log(Boolean(undefined));
+console.log(Boolean(false));
+console.log(Boolean(null));
