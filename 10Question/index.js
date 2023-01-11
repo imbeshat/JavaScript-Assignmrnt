a13a5768ffa9e6c7384663b4a2b0dbefd7daf31c
Imbeshat <md.imbeshat@gmail.com>
2023-01-11 19:30:31 +0530

    fixed solution of question10

--- 10Question/index.js
+++ 10Question/index.js
@@ -12,6 +12,6 @@ console.log(Boolean(4 === "4"));
 
 let py = "python";
 let jar = "jargon";
-console.log(`Lenght of ${py} = `, py.length);
-console.log(`Lenght of ${jar} = `, jar.length);
+console.log(`Length of ${py} = `, py.length);
+console.log(`Length of ${jar} = `, jar.length);
 console.log(Boolean(py.length != jar.length));
