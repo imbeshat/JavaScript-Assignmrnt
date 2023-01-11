a8243b8f9851d7f738f57fe2ea402392df9de611
Imbeshat <md.imbeshat@gmail.com>
2023-01-11 17:13:09 +0530

    added solution for question4

---
+++ 04Question/index.js
@@ -0,0 +1,6 @@
+let str = "Java Script is not a weird language";
+if (str.includes("Script")) {
+	console.log(`${str} is a string which contains the word "Script"`);
+} else {
+	console.log(`${str} is a string which does not contain the word "Script"`);
+}
