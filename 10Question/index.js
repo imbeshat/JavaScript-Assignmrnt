ef46816769ec98a60f39a7b9b7a3941f39eb0147
Imbeshat <md.imbeshat@gmail.com>
2023-01-11 19:29:26 +0530

    added solution for question10

---
+++ 10Question/index.js
@@ -0,0 +1,17 @@
+console.log(Boolean(4 > 3));
+console.log(Boolean(4 >= 3));
+console.log(Boolean(4 < 3));
+console.log(Boolean(4 <= 3));
+console.log(Boolean(4 == 4));
+console.log(Boolean(4 === 4));
+console.log(Boolean(4 != 4));
+console.log(Boolean(4 !== 4));
+console.log(Boolean(4 != "4"));
+console.log(Boolean(4 == "4"));
+console.log(Boolean(4 === "4"));
+
+let py = "python";
+let jar = "jargon";
+console.log(`Lenght of ${py} = `, py.length);
+console.log(`Lenght of ${jar} = `, jar.length);
+console.log(Boolean(py.length != jar.length));
