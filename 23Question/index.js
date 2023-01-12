96fc6af044d5c78ac5ce4cb630c0bfb374f96870
Imbeshat <md.imbeshat@gmail.com>
2023-01-13 01:10:34 +0530

    added solution for question 23

---
+++ 23Question/index.js
@@ -0,0 +1,26 @@
+const readline = require("readline").createInterface({
+	input: process.stdin,
+	output: process.stdout,
+});
+
+readline.question("Enter a number : ", (num) => {
+	let flag = false;
+	if (num == 1) {
+		console.log(`${num} is neither a prime nor a composite number`);
+	} else if (num > 1) {
+		for (let i = 2; i < num / 2; i++) {
+			if (num % i == 0) {
+				flag = true;
+				break;
+			}
+		}
+		if (flag == false) {
+			console.log(`${num} is a prime number`);
+		} else {
+			console.log(`${num} is not a prime number`);
+		}
+	} else {
+		console.log(`${num} is not a prime number`);
+	}
+	readline.close();
+});
