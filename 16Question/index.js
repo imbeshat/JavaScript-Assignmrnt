db0be469c98ebbc09ea34d78aede3a5282cab765
Imbeshat <md.imbeshat@gmail.com>
2023-01-12 14:30:17 +0530

    modified solution of question 16

--- 16Question/index.js
+++ 16Question/index.js
@@ -4,14 +4,15 @@ const readline = require("readline").createInterface({
 });
 
 readline.question("Enter month name : ", (months) => {
-	let month = months.toLowerCase();
-	if (month === "september" || month === "october" || month === "november") {
+	let mon = months.toLowerCase();
+	let month = mon.charAt(0).toUpperCase() + mon.slice(1);
+	if (month === "September" || month === "October" || month === "November") {
 		console.log(`The season is Autumn`);
-	} else if (month === "december" || month === "january" || month === "february") {
+	} else if (month === "December" || month === "January" || month === "February") {
 		console.log(`The season is Winter`);
-	} else if (month === "march" || month === "april" || month === "may") {
+	} else if (month === "March" || month === "April" || month === "May") {
 		console.log(`The season is Spring`);
-	} else if (month === "june" || month === "july" || month === "august") {
+	} else if (month === "June" || month === "July" || month === "August") {
 		console.log(`The season is Summer`);
 	} else {
 		console.log(`${month} is not a valid month.`);
