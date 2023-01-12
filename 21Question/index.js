f82c6a6b3c92fd54223085615684a2abfb1ec51a
Imbeshat <md.imbeshat@gmail.com>
2023-01-13 00:06:24 +0530

    added solution for question 21

---
+++ 21Question/index.js
@@ -0,0 +1,5 @@
+const countries = ["India", "Pakistan", "Sri Lanka", "Bangladesh", "Afghanistan"];
+if (countries.indexOf("Ethiopia") == -1) {
+	countries.push("ETHIOPIA");
+}
+console.log("Country array: ", countries);
