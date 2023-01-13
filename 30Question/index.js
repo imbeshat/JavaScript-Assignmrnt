dbc786c7a1bb4a136593291289019ae48e8d0654
Imbeshat <md.imbeshat@gmail.com>
2023-01-13 21:52:11 +0530

    added solution for question 30

---
+++ 30Question/index.js
@@ -0,0 +1,25 @@
+const readline = require("readline").createInterface({
+	input: process.stdin,
+	output: process.stdout,
+});
+
+readline.question("Enter 1st number : ", (num1) => {
+	readline.question("Enter 2nd number : ", (num2) => {
+		readline.question("Enter operation to be performed : ", (operation) => {
+			if (operation === "+") {
+				console.log(Number(num1) + Number(num2));
+			} else if (operation === "-") {
+				console.log(Number(num1) - Number(num2));
+			} else if (operation === "*") {
+				console.log(Number(num1) * Number(num2));
+			} else if (operation === "/") {
+				console.log(Number(num1) / Number(num2));
+			} else if (operation === "%") {
+				console.log(Number(num1) % Number(num2));
+			} else {
+				console.log("Invalid operation");
+			}
+			readline.close();
+		});
+	});
+});
